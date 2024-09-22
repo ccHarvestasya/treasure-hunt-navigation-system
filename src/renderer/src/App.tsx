574dfa3e339be1091cd6350dd202f28d2f93b2c2
ccHarvestasya <harvestasya84@gmail.com>
2024-09-22 22:35:40 +0900

    1:1固定

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -6,17 +6,22 @@ function App(): JSX.Element {
   const position = new LatLng(33.59337, 130.35152) // 福岡タワー
 
   return (
-    <MapContainer center={position} zoom={13}>
-      <TileLayer
-        attribution='© <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
-        url="https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          PopUp!! PopUp!! PopUp!! <br /> ポップアップ
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      <div className="container">
+        <MapContainer center={position} zoom={13}>
+          <TileLayer
+            attribution='© <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
+            url="https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              PopUp!! PopUp!! PopUp!! <br /> ポップアップ
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+      <p>ここにはなにもないです</p>
+    </>
   )
 }
 
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,9 +3,19 @@ import './assets/main.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
